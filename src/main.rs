eb101eab69faf9005e9cc743c0589c9f3a71296f
Ceri Storey <ceri@lshift.net>
2016-09-15 11:21:01 +0100

    Producer consumer.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate log;
 extern crate env_logger;
 extern crate futures;
-use futures::Future;
+use futures::{Future, Async};
 use futures::task;
 use futures::task::Unpark;
+use futures::stream::{self, Stream, Sender, Receiver};
 
 use std::sync::Arc;
 
@@ -19,15 +20,29 @@ impl Unpark for Unparker {
 
 fn main() {
 
-    let (mut c, mut p) = futures::oneshot();
-    let mut t = task::spawn(p);
-    let mut unparker = Arc::new(Unparker);
+    let (s, r): (Sender<usize, ()>, Receiver<usize, ()>) = stream::channel();
+    let serv = r.for_each(|x| Ok(println!("Recv: {:?}", x)));
+    let unparker = Arc::new(Unparker);
 
+
+    let sfut = futures::finished(s)
+                    .and_then(|s| { println!("Send: {:?}", 1); s.send(Ok(1)) })
+                    .and_then(|s| { println!("Send: {:?}", 2); s.send(Ok(2)) })
+                    .and_then(|s| { println!("Send: {:?}", 3); s.send(Ok(3)) })
+                    .map(|_| ())
+                    .map_err(|_e| println!("Sender error!"));
+    let mut t = task::spawn(sfut.join(serv));
     println!("Setup done!");
-    println!("Poll: {:?}", t.poll_future(unparker.clone()));
 
-    println!("Pre complete");
-    c.complete(42);
-    println!("Post complete");
-    println!("Poll: {:?}", t.poll_future(unparker.clone()));
+    loop {
+        println!("Pre poll");
+        match t.poll_future(unparker.clone()) {
+            Ok(Async::Ready(v)) => {
+                println!("done: {:?}", v);
+                break;
+            }
+            Ok(Async::NotReady) => println!("pending:"),
+            Err(e) => println!("Error: {:?}", e),
+        }
+    }
 }
