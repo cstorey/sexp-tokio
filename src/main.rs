ec5d9a9d9a03457ab98919404f59bd0c94992d87
Ceri Storey <ceri@lshift.net>
2016-09-21 12:50:20 +0100

    First approximation of RPC.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate log;
 extern crate env_logger;
+extern crate tokio_service;
 extern crate futures;
 use futures::{Future, Async};
 use futures::task;
-use futures::task::Unpark;
+use futures::task::{Unpark,Spawn};
 use futures::stream::{self, Stream, Sender, Receiver};
 
+use tokio_service::Service;
 use std::sync::Arc;
 use std::collections::VecDeque;
 
@@ -19,25 +21,48 @@ impl Unpark for Unparker {
     }
 }
 
-fn main() {
+#[derive(Debug)]
+enum Error {}
+struct MyService;
+unsafe impl Send for MyService {}
 
-    let (s, r): (Sender<usize, ()>, Receiver<usize, ()>) = stream::channel();
-    let serv = r.for_each(|x| Ok(println!("Recv: {:?}", x)));
+impl Service for MyService {
+    type Request = u64;
+    type Response = u64;
+    type Error = Error;
+    type Future = futures::Finished<Self::Response, Self::Error>;
+    fn call(&self, req: u64) -> Self::Future {
+        futures::finished(req + 1)
+    }
+    fn poll_ready(&self) -> Async<()> {
+        Async::Ready(())
+    }
+}
+
+fn main() {
+    type Resp = Result<u64, Error>;
+    type Reqs = (u64, futures::Complete<Resp>);
+    let (s, r) : (Sender<Reqs, Error>, Receiver<Reqs, Error>) = stream::channel();
     let unparker = Arc::new(Unparker);
 
+    let client = futures::finished(s)
+        .and_then(|s| {
+            let (c, f) = futures::oneshot();
+            s.send(Ok((42, c))).map_err(|e| panic!("Send error")).map(|s| (s, f))
+        }).and_then(|(s, f)| f)
+        .map_err(|futures::Canceled| panic!("Cancelled"))
+        .map(|r| println!("Result: {:?}", r));
 
-    let input = (0..10).map(Ok).collect::<Vec<Result<usize, ()>>>();
-    let inp = stream::iter(input.into_iter());
-    let sfut = inp.fold(s, |s, n| {
-                      println!("Send: {:?}", n);
-                      s.send(Ok(n)).map_err(|_e| println!("Sender error!"))
-                  })
-                  .and_then(|_| Ok(()));
+    let myserv = MyService;
+//.then(|r| Ok(k.complete(r)))
+    let serv = r
+        .and_then(move |(q, k)| { (myserv.call(q), Ok(k))})
+        .for_each(|(r, k)| Ok(k.complete(Ok(r))));
+
     println!("Setup done!");
-
     let mut tasks = VecDeque::new();
 
-    tasks.push_back(task::spawn(sfut.boxed()));
+    tasks.push_back(task::spawn(client.boxed()));
     tasks.push_back(task::spawn(serv.boxed()));
 
     let unparker = Arc::new(Unparker);
@@ -46,7 +71,6 @@ fn main() {
         match t.poll_future(unparker.clone()) {
             Ok(Async::Ready(v)) => {
                 println!("done: {:?}", v);
-                break;
             }
             Ok(Async::NotReady) => {
                 println!("pending:");
